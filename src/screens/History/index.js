0f5942046d2f45fb4485c7b7c86793a77f3dbee0
Henry Villavicencio <henry@MacBook-Pro-de-Henry.local>
2020-04-19 14:13:45 -0500

    feat: add functions get data user & history

--- src/screens/History/index.js
+++ src/screens/History/index.js
@@ -1,6 +1,24 @@
-import React, { Component } from "react";
+import React, { Component, useEffect } from "react";
 import Layout from "./History.screen";
+import firebase from "../../services/firebase";
+import { useSelector } from "react-redux";
 
 export const History = () => {
+  const { uid } = useSelector((state) => state);
+
+  useEffect(async () => {
+    try {
+      const data = await firebase
+        .firestore()
+        .collection("passenger")
+        .doc(uid)
+        .doc("tickets")
+        .get();
+      console.log(data);
+    } catch (error) {
+      log(error);
+    }
+  }, []);
+
   return <Layout />;
 };
